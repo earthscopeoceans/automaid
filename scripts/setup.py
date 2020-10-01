4515f750830f4b8f3768667ce38c194f1d2f7b59
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2020-10-01 14:29:53 -0700

    Header update

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,8 +1,9 @@
-# automaid setup
+# Part of automaid -- a Python package to process MERMAID files
+# pymaid environment (Python v2.7)
 #
 # Author: Dr. Joel D. Simon
 # Contact: jdsimon@alumni.princeton.edu | joeldsimon@gmail.com
-# Last modified: 30-Sep-2020
+# Last modified: 01-Oct-2020, Python 2.7.15, Darwin-18.7.0-x86_64-i386-64bit
 
 def get_version():
     version = 'v1.2.0'
