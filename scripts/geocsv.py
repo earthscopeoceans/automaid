276d6733a825efac06cbe18a3d611ecb92e27b07
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2020-12-15 13:36:07 -0800

    Define GeoCSV.write()

--- scripts/geocsv.py
+++ scripts/geocsv.py
@@ -14,16 +14,41 @@ class GeoCSV:
     """Organize MERMAID metadata and write them to GeoCSV format
 
     Args:
-        dives (list): List of dives.Dive instances
+        dives (list): List of dives.Dive instances (def: None)
+        version (str): GeoCSV version (def: '2.0')
+        delimiter (str): GeoCSV delimiter (def: ',')
 
     Attributes:
         dives (list): List of dives.Dive instances
 
     """
 
-    def __init__(self, dives=None):
+    def __init__(self, dives=None, version='2.0', delimiter=','):
         self.dives = dives
+        self.version = version
+        self.delimiter = ','
 
+        # Define first two header lines
+        self.dataset_header = '#dataset: GeoCSV ' + self.version
+        self.delimiter_header = '#delimiter: ' + self.delimiter
+
+        
+    def write(self, filename='test.geocsv'):
+        """Write GeoCSV file 
+        
+        Args:
+            filename (str): GeoCSV filename
+
+        """
+        #filename.append('geocsv') if filename.split('.')[-1] != 'geocsv' else pass
+
+        # Open as as 'wb' in Python 2 rather than 'w' with newline='' in Python 3
+        # https://docs.python.org/2/library/csv.html#csv.writer
+        with open(filename, 'wb') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=self.delimiter)
+            csvwriter.writerow([self.dataset_header])
+            csvwriter.writerow([self.delimiter_header])
+                        
 
 if __name__ == "__main__":
     dive_list = pickle.load( open('mdives.p', 'rb'))
