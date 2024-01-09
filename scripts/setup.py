c2b348b0f6d9be1514d4e97533a22fdff2d56c8d
Joel D. Simon (Darwin) <jdsimon@princeton.edu>
2024-01-09 11:07:08 -0800

    Update databases
    Replace databases.py with new one supplied by Frederic Rocca; run
    databases.py to update databases.

--- scripts/setup.py
+++ scripts/setup.py
@@ -19,7 +19,7 @@ def get_version():
 
     """
 
-    return 'v3.6.0-O'
+    return 'v3.6.0-P'
 
 def get_url():
     return 'https://github.com/earthscopeoceans/automaid [doi: 10.5281/zenodo.5057096]'
